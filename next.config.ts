83831cc99f7320b5b2e8894a94e31072eecc6166
Prince Eze <pchidireeze@gmail.com>
2025-03-14 13:49:33 +0100

    feat: inject and update variables for colors and typography

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    additionalData: `@use "@/styles/_variables.scss" as *;`,
+  },
 };
 
 export default nextConfig;
