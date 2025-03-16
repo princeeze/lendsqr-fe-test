5619a13bf78bc674568ffbd473bf1b9d235accd4
Prince Eze <pchidireeze@gmail.com>
2025-03-16 21:32:23 +0100

    feat: implement dashboard layout with navbar and sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,20 +1,29 @@
+"use client";
 import NavBar from "@/components/layout/navbar/navbar";
+import Sidebar from "@/components/layout/sidebar/sidebar";
 import React from "react";
+import styles from "./layout.module.scss";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
   return (
-    <div>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <aside>side bar</aside>
-        <div>{children}</div>
-      </main>
+    <div className={styles.layout}>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <NavBar />
+        </header>
+        <main className={styles.main}>
+          <aside>
+            <Sidebar />
+          </aside>
+          <div style={{ minHeight: "300vh", padding: "40px" }}>{children}</div>
+        </main>
+      </QueryClientProvider>
     </div>
   );
 }
